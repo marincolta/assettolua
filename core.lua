135cd8c8b0cd9c4c28ef41b5464eb2b45218cfee
マリン <marincolta@gmail.com>
2024-07-05 22:18:02 +0300

    Create core.lua

---
+++ core.lua
@@ -0,0 +1,7 @@
+local imagesrc = 'https://cdn.discordapp.com/attachments/1257972518258610177/1258353673780858891/R_no_background_200.png?ex=66890e2e&is=6687bcae&hm=2bf2b38e755c8f81973d00ce4f8d3789841b51cd29545ef76c62def48a5f65fe&'
+local screensize = vec2(ac.getSim().windowWidth,ac.getSim().windowHeight)
+local imageSize,buffer = 200,200
+
+function script.drawUI(dt)
+    ui.drawImage(imagesrc, vec2(buffer,screensize.y-imageSize-buffer), vec2(imageSize+buffer,screensize.y-buffer), false)
+end
