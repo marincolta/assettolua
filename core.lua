60b148f6a3dc31d7507fa69e2c871778965da38c
マリン <marincolta@gmail.com>
2024-07-05 22:18:58 +0300

    Yes
    asd

--- core.lua
+++ core.lua
@@ -1,4 +1,4 @@
-local imagesrc = 'https://cdn.discordapp.com/attachments/1257972518258610177/1258353673780858891/R_no_background_200.png?ex=66890e2e&is=6687bcae&hm=2bf2b38e755c8f81973d00ce4f8d3789841b51cd29545ef76c62def48a5f65fe&'
+local imagesrc = 'https://cdn.discordapp.com/attachments/1257972518258610177/1258353673780858891/R_no_background_200.png?ex=6680e2e&is=6687bcae&hm=2bf2b38e755c8f81973d00ce4f8d3789841b51cd29545ef76c62def48a5f65fe&'
 local screensize = vec2(ac.getSim().windowWidth,ac.getSim().windowHeight)
 local imageSize,buffer = 200,200
 
